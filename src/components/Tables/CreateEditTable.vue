90493e01c3bd115fa5c483c1d5e55793f61b4a65
sriramya <sriramya.java@gmail.com>
2018-09-17 14:15:12 -0400

    router navigation gaurds

--- src/components/Tables/CreateEditTable.vue
+++ src/components/Tables/CreateEditTable.vue
@@ -73,6 +73,14 @@
       this.id = this.$route.params.tableString
       // console.log(this.id)
     },
+    beforeRouteLeave (to, from, next) {
+      const answer = window.confirm('Do you really want to leave? you have unsaved changes!')
+      if (answer) {
+        next()
+      } else {
+        next(false)
+      }
+    },
     methods: {
       navigate () {
         router.go(-1)
